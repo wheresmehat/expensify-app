7b19aa0cdcaa449cb3b9cf5186bd36e000eb12ec
wheresmehat <csstogo@gmail.com>
2017-10-27 15:05:50 +0200

    dist folder added

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = (env) => {
 
         output: {
 
-            path: path.join(__dirname, "public"),
+            path: path.join(__dirname, "public", "dist"),
             filename: "bundle.js" 
         },
 
@@ -56,7 +56,8 @@ module.exports = (env) => {
         devServer: {
 
             contentBase: path.join(__dirname, "public"),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: "/dist/"
         }
 
     };
